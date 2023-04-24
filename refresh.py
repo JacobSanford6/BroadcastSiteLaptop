008d0d1884ec0aef9bdbb423e2b7bb8da3637a3a
Jake Sanford <jacob.r.sanford@gmail.com>
2023-04-23 20:19:55 -0500

    auto refresh

---
+++ refresh.py
@@ -0,0 +1,24 @@
+import requests
+import time
+from selenium import webdriver
+
+
+
+lastRaw=  None
+driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
+driver.implicitly_wait(0.5)
+
+driver.get("http://167.248.46.73:5000")
+
+def refreshPage():
+    global driver
+    driver.refresh()
+
+while True:
+    time.sleep(.5)
+    rq = requests.get("http://167.248.46.73:5000")
+    if lastRaw != rq.text:
+        refreshPage()
+    lastRaw = rq.text
+    rq.close()
+    
