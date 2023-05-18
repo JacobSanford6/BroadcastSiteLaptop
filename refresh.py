3659138a889688a50e3a7ade9c45836274d1020d
Jake Sanford <jacob.r.sanford@gmail.com>
2023-05-17 20:26:47 -0500

    finished

--- refresh.py
+++ refresh.py
@@ -5,18 +5,23 @@ from selenium import webdriver
 
 
 lastRaw=  None
+url = "http://jacobsanford.ddns.net:5000"
+
 driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")
 driver.implicitly_wait(0.5)
 
-driver.get("http://jacobsanford.ddns.net:5000")
+driver.get(url)
 
+# Refreshes display
 def refreshPage():
     global driver
     driver.refresh()
 
+# Check if website has changed every half second
+# If website has changed, refresh the page
 while True:
     time.sleep(.5)
-    rq = requests.get("http://167.248.46.73:5000")
+    rq = requests.get(url)
     if lastRaw != rq.text:
         refreshPage()
     lastRaw = rq.text
